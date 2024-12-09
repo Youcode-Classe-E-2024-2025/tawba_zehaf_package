94f310540513a072a289c31d63eb338b4987f871
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2024-12-09 15:00:51 +0100

    debut de travail sur page admin

---
+++ php/admin.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
+    header('Location: login.php');
+    exit();
+}
+require './db.php';
+
+
+
+
+
+
+
+
+
+
+?>
