7c5147f83f665f9e0578ab237552e518aa096515
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2024-12-10 12:20:48 +0100

    modofication de  jointure pourque l user et l admin peut y acceder a la page

--- php/packages.php
+++ php/packages.php
@@ -1,5 +1,4 @@
 <?php
-
 session_start();
 require ' php\db.php'; 
 // Initialiser les packages si non définis
--- php/user.php
+++ php/user.php
@@ -5,16 +5,16 @@ require './db.php';
 $query = $_GET['query'] ?? '';
 
 // Fetch packages with their version and authors
+
 $stmt = $pdo->prepare("
-    SELECT p.id_package, p.nom_package, p.description, 
-       MAX(v.version) AS version, GROUP_CONCAT(a.nom_auteur) AS authors
-    FROM Packages p
-    LEFT JOIN Versions v ON p.id_package = v.id_package
-    LEFT JOIN authors ap ON p.id_package = ap.id_package
-    LEFT JOIN authors a ON ap.id_auteur = a.id_auteur
-    WHERE p.nom_package LIKE :query OR p.description LIKE :query
-    GROUP BY p.id_package, p.nom_package, p.description
-    ORDER BY p.nom_package
+    SELECT p.id_packages, p.name AS nom_package, p.description, 
+           MAX(v.version_number) AS version, GROUP_CONCAT(a.name) AS authors
+    FROM packages p
+    LEFT JOIN versions v ON p.id_packages = v.package_id
+    LEFT JOIN authors a ON p.author_id = a.id_authors
+    WHERE p.name LIKE :query OR p.description LIKE :query
+    GROUP BY p.id_packages, p.name, p.description
+    ORDER BY p.name
 ");
 $stmt->execute(['query' => "%$query%"]);
 $packages = $stmt->fetchAll();
