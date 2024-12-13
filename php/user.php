402f4603442b5e7958506f178836e516b3a29a11
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2024-12-13 15:57:29 +0100

    FIXING the jointure for the fourth time cz it was showinf

--- php/user.php
+++ php/user.php
@@ -4,14 +4,15 @@ require './db.php';
 $query = $_GET['query'] ?? '';
 $stmt = $pdo->prepare("
     SELECT p.id_package, p.nom_package, p.description, 
-           MAX(v.version) AS version, GROUP_CONCAT(a.nom_auteur) AS authors
-    FROM packages p
-    LEFT JOIN versions v ON p.id_package = v.id_package
-    LEFT JOIN auteurs_packages ap ON p.id_package = ap.id_package
-    LEFT JOIN auteurs a ON ap.id_auteur = a.id_auteur
-    WHERE p.nom_package LIKE :query OR p.description LIKE :query
-    GROUP BY p.id_package, p.nom_package, p.description
-    ORDER BY p.nom_package
+       MAX(v.version) AS version, GROUP_CONCAT(DISTINCT a.nom_auteur) AS authors
+FROM packages p
+LEFT JOIN versions v ON p.id_package = v.id_package
+LEFT JOIN auteurs_packages ap ON p.id_package = ap.id_package
+LEFT JOIN auteurs a ON ap.id_auteur = a.id_auteur
+WHERE p.nom_package LIKE :query OR p.description LIKE :query
+GROUP BY p.id_package, p.nom_package, p.description
+ORDER BY p.nom_package
+
 ");
 
 $stmt->execute(['query' => "%$query%"]);
